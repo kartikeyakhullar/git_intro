3879b9e2c8006ffc686a1921728ef59c849f80a8
Kartikeya <kartikeya.knightriders@gmail.com>
2019-03-07 00:01:38 +0530

    Grateful message added.

--- file02.js
+++ file02.js
@@ -1,7 +1,8 @@
 function greet(name){
     console.log('Hello knock knock knock ' + name);
+    console.log('We are grateful to have you here.');
 }
 
 // console.log('Hey everybody. I am learning git.')
 
-greet('Kartikeya');
+greet('Kartikeya'); 
